8549289b28de805f70edc43cefa3f6bd18f66892
Zwelethu Junior Nkosi <zozwejr@gmail.com>
2023-03-20 15:07:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ *main - this is the main function
+ *
+ *Return: 0 When successful
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
