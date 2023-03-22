1f435cfee630aa7ad4c2527c98e9babf56178a9d
Zwelethu Junior Nkosi <zozwejr@gmail.com>
2023-03-22 11:42:47 -0700

    here goes 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main function
+ * _putchar: puts a character
+ * Return: 0 when finished
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
